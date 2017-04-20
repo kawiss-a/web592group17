9f72d0a825faa56c8f4c2b2ea85de1cf0dd771d0
jungqueen <jungqueen9@gmail.com>
2017-04-20 19:34:06 +0700

    Add files via upload

--- main.php
+++ main.php
@@ -1,3 +1,52 @@
-<?php
-	include("index.html");
-?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">
+<html>
+<HEAD>
+ <meta charset="utf-8">
+<TITLE>BlackShadowSeries</TITLE>
+<meta http-equiv=Content-Type content="text/html; charset=windows-874">
+<meta name="google-site-verification" content="m0Eg0sEUEEW1HjezVyZFuTZ4WIzqPFXSDXWz0QRMunU" />
+<STYLE>
+
+.BTN-A
+{
+   font-size:24px;
+   background-color:#339933;
+   color:#ffffff;
+   border :solid 2px #000;
+   text-decoration:none;
+   padding: 3px 3px 3px 3px;
+}
+.BTN-B
+{
+   font-size:24px;
+   color:#ffffff;
+   background-color:#cc0033;
+   border :solid 2px #000;
+   text-decoration:none;
+    padding: 3px 3px 3px 3px;
+}
+
+</STYLE>
+</HEAD>
+<body background = "bg.jpg"></body>
+<CENTER><BR>
+ <div style="background-color:#FFFFFF;">
+  
+ </div>
+ <h1>แนะนำซีรีย์</h1>
+ <BR><BR>
+ <div class="carousel-inner">
+ <!-- Indicators -->
+                        <div class="item active">
+                                <img class="img-responsive img-full" src="Go.jpg" alt="">
+                            </div>
+                            
+                        </div>
+  <TABLE width='50%' align='center'>
+    <TR>
+	 <TD  align='center'><a class='BTN-B' href="http://web592group17.appspot.com/index.html">เข้าสู่เว็บไซต์</a></TD>
+	</TR>
+  </TABLE>
+</CENTER>
+</BODY>
+</html>
